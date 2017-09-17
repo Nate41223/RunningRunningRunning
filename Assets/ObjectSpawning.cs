d4399b854939b7114c64f5983272672ab185dbd3
Unknown <Stenman100@Gmail.com>
2017-09-17 19:31:11 -0400

    PowerUps Added
    Added PowerUps and am saving before i change lane amount

--- Assets/ObjectSpawning.cs
+++ Assets/ObjectSpawning.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class ObjectSpawning : MonoBehaviour {
 
-    public GameObject LeftBackSpawnPoint;
-    public GameObject LeftFrontSpawnPoint;
-    public GameObject MiddleBackSpawnPoint;
-    public GameObject MiddleFrontSpawnPoint;
-    public GameObject RightBackSpawnPoint;
-    public GameObject RightFrontSpawnPoint;
+    // Spawn Areas
+    public GameObject LeftBackSpawnPoint;  // List Index Number: 0
+    public GameObject LeftFrontSpawnPoint; // List Index Number: 1
+    public GameObject MiddleBackSpawnPoint; // List Index Number: 2
+    public GameObject MiddleFrontSpawnPoint; // List Index Number: 3
+    public GameObject RightBackSpawnPoint; // List Index Number: 4
+    public GameObject RightFrontSpawnPoint; // List Index Number: 5
 
-    public GameObject Wall;
+    // Obstacles
+    public GameObject Wall; // List Index Number: 0
+    public GameObject PointsPowerUp; // List Index Number: 1
+    public GameObject BreakPowerUp; // List Index Number: 2
+    public GameObject SlowPowerUp; // List Index Number: 3
+    public GameObject AttractPowerUp; // List Index Number: 4
+
 
     List<GameObject> Points = new List<GameObject>();
+    List<GameObject> Obstacles = new List<GameObject>();
 
     // Use this for initialization
     public void spawnObjects () {
 
+        // Adds all Spawn Areas to the Points List
         Points.Add(LeftBackSpawnPoint);
         Points.Add(LeftFrontSpawnPoint);
         Points.Add(MiddleBackSpawnPoint);
@@ -25,17 +34,43 @@ public class ObjectSpawning : MonoBehaviour {
         Points.Add(RightBackSpawnPoint);
         Points.Add(RightFrontSpawnPoint);
 
-        print(Points.Count);
+        // Adds all Objects to the Obstacles List
+        Obstacles.Add(Wall);
+        Obstacles.Add(PointsPowerUp);
+        Obstacles.Add(BreakPowerUp);
+        Obstacles.Add(SlowPowerUp);
+        Obstacles.Add(AttractPowerUp);
 
         // gets the amount of objects it wants to spawn for that chunk
-        int amount = Random.Range(0, 7); // max is exclusive
+        int amount = Random.Range(1, Points.Count + 1); // max is exclusive
+        int amountOfPowerUp = 1;
+
+        print(amount);
 
         while (amount > 0)
         {
-            int spawnLocation = Random.Range(0, Points.Count);
+            // determines the spawn location of the object
+            int spawnLocation = Random.Range(0, Points.Count); // max is exclusive
             Vector3 position = Points[spawnLocation].transform.position;
-            var newWall = Instantiate(Wall, position, Quaternion.identity);
-            newWall.transform.parent = transform;
+
+            // determines if a powerup object can replace a wall object in this position
+            int isPowerUp = Random.Range(0, 101); // max is exclusive
+
+            // if isPowerUp is less than 25 AND we havnt spawned a powerup on this trackpiece yet, then spawn a powerup in this location
+            // OR if this is the last object spawning on this track piece AND we havnt spawned a powerup on this trackpiece yet, then spawn a powerup in this location
+            // ELSE spawn a wall in this location
+            if (isPowerUp < 25 && amountOfPowerUp != 0 || amount != 1 && amountOfPowerUp != 0)
+            {
+                var newPowerUp = Instantiate(determinePowerUp(), position, Quaternion.identity);
+                newPowerUp.transform.parent = transform;
+                amountOfPowerUp = 0;
+            } else
+            {
+                var newWall = Instantiate(Wall, position, Quaternion.identity);
+                newWall.transform.parent = transform;
+            }
+
+            // remove this point from the List of points so that only one object can spawn in that location
             Points.RemoveAt(spawnLocation);
             amount--;
             
@@ -46,4 +81,9 @@ public class ObjectSpawning : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public GameObject determinePowerUp()
+    {
+        return Obstacles[Random.Range(1, Obstacles.Count)]; // max is exclusive
+    }
 }
